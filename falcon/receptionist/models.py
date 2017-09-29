8ddef0b0ba8ab9a577b20142e12d72fb26a34b86
jasv95 <32079188+jasv95@users.noreply.github.com>
2017-09-29 18:20:10 -0400

    Doc models

--- falcon/receptionist/models.py
+++ falcon/receptionist/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Appointment (models.Model):
     #patient_ID= models. Add forign key for patient id
     symptoms = models.CharField(max_length=1000)
