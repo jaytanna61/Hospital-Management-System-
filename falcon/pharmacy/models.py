48e8ed74cabe9588e743e109647d7679b4ab9c6a
karansheth <karan.sheth60@gmail.com>
2017-09-29 19:01:32 -0400

    Foreign Keys Added

--- falcon/pharmacy/models.py
+++ falcon/pharmacy/models.py
@@ -1,10 +1,6 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 # Create your models here.
-
-
-class Product(models.Model):
-    product_name = models.CharField(max_length=70)
-    manufacturer = models.ForeignKey(Manufacturer.id)
 
 
 class Manufacturer(models.Model):
@@ -15,24 +11,35 @@ class Manufacturer(models.Model):
     email = models.EmailField
 
 
-class Batch(models.Model):
-    product = models.ForeignKey(Product.id)
-    supplier = models.ForeignKey(Supplier.id)
-    count = models.DecimalField(decimal_places=2)
-    available_count = models.DecimalField(decimal_places=2)
-    manufacturing_date = models.DateField
-    expire_date = models.DateField
-    cost = models.DecimalField(decimal_places=2)
-    purchase_date = models.DateField
-    power = models.DecimalField(decimal_places=4)
-
-
 class Supplier(models.Model):
     supplier_name = models.CharField(max_length=70)
     location = models.CharField(max_length=250)
     phone_number = models.CharField(max_length=15)
     address = models.TextField
     email = models.EmailField
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=70)
+    manufacturer = models.ForeignKey(Manufacturer)
+
+
+class Batch(models.Model):
+    product = models.ForeignKey(Product)
+    supplier = models.ForeignKey(Supplier)
+    count = models.DecimalField(max_digits=10, decimal_places=2)
+    available_count = models.DecimalField(max_digits=10, decimal_places=2)
+    manufacturing_date = models.DateField
+    expire_date = models.DateField
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    purchase_date = models.DateField
+    power = models.DecimalField(max_digits=10, decimal_places=4)
+
+
+
+
+
+
 
 
 
