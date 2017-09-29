3dee7751d31259943ed32b20af5e4521b572cf03
jasv95 <32079188+jasv95@users.noreply.github.com>
2017-09-29 18:25:07 -0400

    doc second commit

--- falcon/doctor/models.py
+++ falcon/doctor/models.py
@@ -8,7 +8,6 @@ from receptionist.models import Appointment
 class Patient(models.Model):
     name = models.CharField(max_length=70)
     dob = models.DateField
-    contact_no = models.IntegerField(max_length=10)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                  message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=15, blank=True)  # validators should be a list
