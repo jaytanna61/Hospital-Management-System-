537deddcfeeabbceaeda4c0679da346d0363e947
karansheth <karan.sheth60@gmail.com>
2017-09-30 12:49:33 -0400

    Foreign Key for Appointment Working

--- falcon/receptionist/models.py
+++ falcon/receptionist/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
+#from django.contrib.auth.models import User
 # Create your models here.
 from django.template.defaultfilters import default
 
 
 class Appointment (models.Model):
-    #patient= models.ForeignKey(User)
+    patient = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
     symptoms = models.CharField(max_length=1000)
     date = models.DateTimeField
